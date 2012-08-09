ae8a3bc9a70c2556e1395aa0ac63909e2ceb55d2
fernanda-andrade <fernanda.andrade@prodap.com.br>
2012-08-09 09:46:48 -0300

    att final

--- quest/views.py
+++ quest/views.py
@@ -14,18 +14,21 @@ def quest(request):
 
 def quest_nutri(request):
     data = {}
+    #data["empresa"] = nutri
     data["BASE_URL"] = settings.BASE_URL
     return render(request, 'quest-nutri.html', data)
 
 
 def quest_pg(request):
     data = {}
+    #data["empresa"] = pg
     data["BASE_URL"] = settings.BASE_URL
     return render(request, 'quest-pg.html', data)
 
 
 def quest_tech(request):
     data = {}
+    #data["empresa"] = tech
     data["BASE_URL"] = settings.BASE_URL
     return render(request, 'quest-tech.html', data)
 
@@ -46,8 +49,23 @@ def resp(request):
 
     for p in range(0, perguntas + 1):
         respostas = []
+        #respostas.append(empresa)
         respostas.append(data_atual)
         respostas.append(usuario)
+
+        for e in range(1, 5):
+            key = "p%se%s" % (str(p), str(e))
+            respostas.append(request.POST[key])
+
+        csv_out.append(respostas)
+
+    arquivo_csv = open('output.csv', "a")
+    writer = csv.writer(arquivo_csv, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
+    for r in csv_out:
+        writer.writerow(r)
+    data['success'] = True
+    return render(request, 'resp.html', data)
+
 
         for e in range(1, 5):
             key = "p%se%s" % (str(p), str(e))
